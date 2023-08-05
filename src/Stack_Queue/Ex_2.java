90ecd996175279e4aa4acf50565cc39549fba510
didgh <didghtls11@gmail.com>
2023-08-05 17:06:17 +0900

    08.05 스택, 큐 공부

---
+++ src/Stack_Queue/Ex_2.java
@@ -0,0 +1,26 @@
+package Stack_Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * 실버4 (백준 2164)
+ */
+
+public class Ex_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> myQueue = new LinkedList<>();
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            myQueue.add(i);
+        }
+
+        while (myQueue.size() > 1) {
+            myQueue.poll();
+            myQueue.add(myQueue.poll());
+        }
+        System.out.println(myQueue.poll());
+    }
+}
