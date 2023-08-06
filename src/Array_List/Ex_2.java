2a247e0769912afebe67ca962c9df2c5c934ddec
didgh <didghtls11@gmail.com>
2023-08-06 21:36:41 +0900

    23.08.06 정렬 알고리즘 공부

--- src/Array_List/Ex_2.java
+++ src/Array_List/Ex_2.java
@@ -11,12 +11,12 @@ import java.util.Scanner;
 public class Ex_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int score = sc.nextInt();
+        int score = sc.nextInt();   //시험 본 과목의 개수 - int 입력
         long sum = 0;
         long max = 0;
 
         for (int i = 0; i < score; i++) {
-            int temp = sc.nextInt();
+            int temp = sc.nextInt();    //현재 성적 - int 입력
             if (temp > max) max = temp;
             sum = sum + temp;
         }
