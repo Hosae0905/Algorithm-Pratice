a5003c23f01df7c60bca3391e8fbcd08e96d3e08
didgh <didghtls11@gmail.com>
2023-08-13 17:28:08 +0900

    23.08.13 조합, DP 공부

---
+++ src/Combination/Ex_1.java
@@ -0,0 +1,30 @@
+package Combination;
+
+import java.util.Scanner;
+
+/**
+ * 브론즈 1 (백준 11050)
+ */
+
+public class Ex_1 {
+    static int N, K;
+    static int D[][];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        D = new int[N+1][N+1];
+        for (int i = 0; i <= N; i++) {
+            D[i][i] = 1;
+            D[i][0] = 1;
+            D[i][1] = i;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                D[i][j] = D[i - 1][j] + D[i - 1][j-1];
+            }
+        }
+        System.out.println(D[N][K]);
+    }
+}
---
+++ src/Dynamic_Programming/Ex_1.java
@@ -0,0 +1,22 @@
+package Dynamic_Programming;
+
+import java.util.Scanner;
+
+/**
+ *  실버3 (백준 11726)
+ */
+
+public class Ex_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long D[] = new long[1001];
+        D[1] = 1;
+        D[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            D[i] = (D[i - 1] + D[i - 2]) % 10007;
+        }
+        System.out.println(D[N]);
+    }
+}
